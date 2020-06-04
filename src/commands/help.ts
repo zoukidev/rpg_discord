0ee82d51a741df5d2a5054b4e91576cce9d108e9
Zouki'Dev <zouki.dev@gmail.com>
2020-06-04 07:41:55 +0200

    Update HelpCommand

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -7,7 +7,14 @@ export default class HelpCommand extends CommandModel {
     }
 
     exec(msg: Discord.Message) {
-        msg.reply(`${this.name} executed!`);
+        let textHelp = 'commands list:';
+        textHelp += '```';
+        textHelp += '!help - commands list\n';
+        textHelp += '!mine - harvest resources\n';
+        textHelp += '!ressources - ressources list\n';
+        textHelp += '!money - display money';
+        textHelp += '```';
+        msg.reply(textHelp);
         msg.delete();
     }
 }
