c563394099f9ab1a99119d99f20216cb7998dedf
Zouki'Dev <zouki.dev@gmail.com>
2020-06-06 01:59:54 +0200

    Update documentation comment for CommandModel, Update package.json license.

--- src/commands/model.ts
+++ src/commands/model.ts
@@ -4,6 +4,11 @@ export default class CommandModel {
     name: string;
     description: string;
 
+    /**
+     *
+     * @param name
+     * @param desc
+     */
     constructor(name: string, desc: string) {
         this.name = name;
         this.description = desc;
